491a86e95743c5bb25d52e1e704ac927f0877dfb
mohamedamr241 <medo.amr2001@gmail.com>
2022-08-23 19:04:35 +0200

    My sec page.

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
+export const sequelize = new Sequelize(
+  `postgres://${config.username}:${config.password}@${config.host}:${config.DB_PORT}/${config.database}`
+);
 
-  dialect: "postgres",
-  storage: ":memory:",
-});
+  
